910be6a9764908cacf170e34461dadd84099ef05
Kostas Kaplanis <kostaskaplanis2001@hotmail.com>
2023-02-24 15:27:09 +0200

    SignIn page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+import'../node_modules/bootstrap/dist/css/bootstrap.min.css'
+ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
+export default MyApp
